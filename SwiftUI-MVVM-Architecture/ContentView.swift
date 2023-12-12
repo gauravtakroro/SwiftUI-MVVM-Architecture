4ee0ac4fe9f7fd83bcc4f0746793224780cc58a0
Gaurav Tak <gaurav.tak@roro.io>
2023-12-13 00:04:06 +0530

    ContentView ui updated

--- SwiftUI-MVVM-Architecture/ContentView.swift
+++ SwiftUI-MVVM-Architecture/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("swiftui_icon1").resizable()
+                .frame(width: 60, height: 60)
+            Text("SwiftUI with MVVM Architecture")
         }
-        .padding()
+        .scaleEffect()
+
     }
 }
 
