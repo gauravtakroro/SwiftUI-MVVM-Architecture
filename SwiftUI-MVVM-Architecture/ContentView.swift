e6fb449e329e09f23bd953d55317f98bfabcd8c6
Gaurav Tak <gaurav.tak@roro.io>
2023-12-12 23:24:52 +0530

    Initial Commit

---
+++ SwiftUI-MVVM-Architecture/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM-Architecture
+//
+//  Created by Roro Solutions LLP on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-MVVM-Architecture/SwiftUI_MVVM_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_ArchitectureApp.swift
+//  SwiftUI-MVVM-Architecture
+//
+//  Created by Roro Solutions LLP on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
